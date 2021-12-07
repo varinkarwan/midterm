7c3d6d17eeeb544333ba91f4f27ec303ed89ba2f
varinkarwan <varinkarwan2121@gmail.com>
2021-12-07 13:59:56 +0300

    i create the note class

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,3 +1,35 @@
 //! Define the model class Note here
 
-class Note {}
+class Note {
+    int id;
+    String title;
+    String content;
+    String noteColor;
+
+    Note({
+        this.id = null, 
+        this.title = "Note", 
+        this.content = "Text", 
+        this.noteColor = 'red'
+    });
+
+    Map<String, dynamic> toMap() {
+        Map<String, dynamic> data = Map<String, dynamic>();
+        if (id != null) {
+            data['id'] = id;
+        }
+        data['My notes'] = title;
+        data['content'] = content;
+        data['noteColor'] = noteColor;
+        return data;
+    }
+
+    @override toString() {
+        return {
+            'id': id,
+            'title': title,
+            'content': content,
+            'noteColor': noteColor,
+        }.toString();
+    }
+}
